1435e473961981bc4e5be3c1f56cd600f9d8c74f
Patrick Keane <pkeane92@yahoo.com>
2019-02-04 16:37:42 -0600

    can use scraper to get title and summary from some articles on Vox homepage

---
+++ server.js
@@ -0,0 +1,29 @@
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+axios.get("https://www.vox.com/").then(function(response){
+
+var $ = cheerio.load(response.data);
+
+var articles = [];
+
+$(".c-entry-box--compact").each(function(i, element){
+    
+    var entry = {};
+    // var entry = $(this).html();
+    entry.title = $(element)
+    .find("h2").text();
+    entry.summary = $(element)
+    .find(".p-dek").text();
+    // .find("div.c-entry-box--compact_body").html();
+    // .find("a").text().;
+    // .find($(".c-entry-box--compact_title")).html();
+    // .find(".c-entry-box--compact_body").children().html();
+    // .find("h2.c-entry-box--compact_title")
+
+    articles.push(entry);
+});
+
+console.log(articles);
+
+})
