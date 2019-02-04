c5d76166b0a964292e6c4aefe46b74add55137c7
Patrick Keane <pkeane92@yahoo.com>
2019-02-04 17:10:07 -0600

    made it so that only entries with a summary are included and got link for each article

--- server.js
+++ server.js
@@ -11,17 +11,21 @@ $(".c-entry-box--compact").each(function(i, element){
     
     var entry = {};
     // var entry = $(this).html();
-    entry.title = $(element)
-    .find("h2").text();
-    entry.summary = $(element)
-    .find(".p-dek").text();
+    entry.title = $(element).find("h2").text();
+    entry.summary = $(element).find(".p-dek").text();
+    entry.link = $(element).find("a").attr("href");
     // .find("div.c-entry-box--compact_body").html();
     // .find("a").text().;
     // .find($(".c-entry-box--compact_title")).html();
     // .find(".c-entry-box--compact_body").children().html();
     // .find("h2.c-entry-box--compact_title")
 
-    articles.push(entry);
+    if (((entry.summary) !== "") && ((entry.summary) !== null)) {
+
+        articles.push(entry);
+
+    }
+
 });
 
 console.log(articles);
