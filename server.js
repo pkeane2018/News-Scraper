45101854d2829984289983ad6785f921267c5e9f
Patrick Keane <pkeane92@yahoo.com>
2019-02-06 17:23:21 -0600

    did a bunch of stuff

--- server.js
+++ server.js
@@ -38,15 +38,9 @@ app.get("/home", function (req, res) {
         $(".c-entry-box--compact").each(function (i, element) {
 
             var entry = {};
-            // var entry = $(this).html();
             entry.title = $(element).find("h2").text();
             entry.summary = $(element).find(".p-dek").text();
             entry.link = $(element).find("a").attr("href");
-            // .find("div.c-entry-box--compact_body").html();
-            // .find("a").text().;
-            // .find($(".c-entry-box--compact_title")).html();
-            // .find(".c-entry-box--compact_body").children().html();
-            // .find("h2.c-entry-box--compact_title")
 
             if (((entry.summary) !== "") && ((entry.summary) !== null)) {
 
@@ -73,6 +67,18 @@ app.get("/home", function (req, res) {
         .catch(function(err){
             console.log(err);
         })
+    })
+});
+
+app.delete("/comment/:commentid/:articleid", function(req, res){
+
+    db.Comment.findByIdAndDelete(req.params.commentid)
+    .then(function(){
+        console.log("This article id is" + req.params.articleid)
+        db.Article.update({_id: req.params.articleid}, { $pull: {comment: "ObjectId(" + '"' + req.params.commentid + '"' + ")"}})
+    })
+    .then(function(){
+        console.log("Comment deleted");
     })
 });
 
